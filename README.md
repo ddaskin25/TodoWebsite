b4ce48097066c01aa0b96ebcc359c393acfb3a1c
Davud Daskin <davuddaskin@gmail.com>
2016-12-12 11:30:38 +0100

    Added all files of the assignment

---
+++ README.md
@@ -0,0 +1,3 @@
+# TodoBox
+
+A live preview can be found [here](https://adriandmen.github.io/TodoBox/).
---
+++ scripts.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $(".responsive-menu").click(function() {
+        $(".dropdown-menu").slideToggle(150);
+    });
+});
